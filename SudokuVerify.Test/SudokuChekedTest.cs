8f0fab0d9bb50c4b973d47986cd2e378d3ab3bea
Patrick <patrick.martins@madefy.com.br>
2022-11-06 13:56:25 -0300

    xUnit test

--- SudokuVerify.Test/SudokuChekedTest.cs
+++ SudokuVerify.Test/SudokuChekedTest.cs
@@ -16,11 +16,14 @@ namespace SudokuVerify.Test
             _repository = new Mock<ISudokuCheckedRepository>();
             _sudokuCheckedService = new SudokuCheckedService(_repository.Object);
         }
-      
 
-        [Fact]
-        public void IsSudokuOk()
+        [Theory]
+        [InlineData(3,3,8, true)]
+        [InlineData(1,5,7, true)]     
+        [InlineData(3,4,3, true)]     
+        public void IsSudokuOk_SudokuVerify_ReturnTrue(int row, int col, int value, bool expected)
         {
+            //Arrange
             string[] sudoku = {
                  "534678912",
                  "672195348",
@@ -32,7 +35,36 @@ namespace SudokuVerify.Test
                  "287419635",
                  "345286179"};
 
-            Assert.True(_sudokuCheckedService.IsSudokuOk(sudoku, 3, 3, 8));
+            //Act
+            var validation = _sudokuCheckedService.IsSudokuOk(sudoku, row, col, value);
+
+            //Assert
+            Assert.Equal(expected, validation);
+        }
+
+        [Theory]
+        [InlineData(3, 3, 2, false)]
+        [InlineData(1, 5, 4, false)]
+        [InlineData(3, 4, 9, false)]
+        public void IsSudokuOk_SudokuVerify_ReturnFalse(int row, int col, int value, bool expected)
+        {
+            //Arrange
+            string[] sudoku = {
+                 "534678912",
+                 "672195348",
+                 "198342567",
+                 "859761423",
+                 "426853791",
+                 "713924856",
+                 "961537284",
+                 "287419635",
+                 "345286179"};
+
+            //Act
+            var validation = _sudokuCheckedService.IsSudokuOk(sudoku, row, col, value);
+
+            //Assert
+            Assert.Equal(expected, validation);
         }
     }
 }
